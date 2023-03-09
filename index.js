0113a60d827faad89ec092b7debe1d7ad5d6fdd5
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2023-03-09 10:58:17 +0300

    Get element by id

---
+++ index.js
@@ -0,0 +1,7 @@
+// First argument - event
+// Second argument - callback function
+document.addEventListener('DOMContentLoaded', () => {
+	const header = document.getElementById('greeting');
+
+	console.log(header);
+});
